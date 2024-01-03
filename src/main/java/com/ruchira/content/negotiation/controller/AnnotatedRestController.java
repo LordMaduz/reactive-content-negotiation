ad3b519297ca12c84186747000b09845ae4e03e8
LordMaduz <maduz.ruchira@gmail.com>
2024-01-03 12:03:07 +0800

    commit api path changes.

--- src/main/java/com/ruchira/content/negotiation/controller/AnnotatedRestController.java
+++ src/main/java/com/ruchira/content/negotiation/controller/AnnotatedRestController.java
@@ -1,18 +1,21 @@
 package com.ruchira.content.negotiation.controller;
 
+import com.ruchira.content.negotiation.constant.EndPointConstant;
 import com.ruchira.content.negotiation.vo.RequestVo;
 import com.ruchira.content.negotiation.vo.ResponseVo;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 
 @RestController
+@RequestMapping(EndPointConstant.ANNOTATED_CONTROLLER_API_PATH)
 public class AnnotatedRestController {
 
-    @PostMapping(value = "/annotation-based-api")
+    @PostMapping()
     public Mono<ResponseVo> annotationBasedAPI(@RequestBody Mono<RequestVo> requestVoMono) {
         return requestVoMono.map(requestVo -> {
             final ResponseVo  responseVo = new ResponseVo();
